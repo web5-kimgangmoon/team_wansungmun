3e9995a5a623a204f229c08d6e6fbdd9f098a83e
kimgangmoon <kimgangmoon>
2024-07-15 10:47:53 +0900

    feat:frontSetting

--- wansungmoon_team/src/index.tsx
+++ wansungmoon_team/src/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </RecoilRoot>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ wansungmoon_team/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+    },
+    screens: {
+      "sm": "0px",
+      "md":"321px",
+      "lg":"769px",
+      "xl":"1401px"
+      // "wd":{
+      //   "bs": "(min-width:320px)",
+      //   "ml":"(min-width:768px)",
+      //   "big":"(min-width:1400px)",
+      //   "mx":"(min-width:1920px)"
+      // }
+
+    },
+  },
+  plugins: [],
+};
