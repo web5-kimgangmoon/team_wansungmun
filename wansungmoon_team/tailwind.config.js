bb7f10031cb29c1affb4dc9873c717c1963ed962
JiwanBang <jiwanbang@gmail.com>
2024-07-15 12:00:22 +0900

    tailwind_css_config

--- wansungmoon_team/tailwind.config.js
+++ wansungmoon_team/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       container: {
@@ -8,17 +8,16 @@ module.exports = {
       },
     },
     screens: {
-      "sm": "0px",
-      "md":"321px",
-      "lg":"769px",
-      "xl":"1401px"
+      sm: "0px",
+      md: "321px",
+      lg: "769px",
+      xl: "1401px",
       // "wd":{
       //   "bs": "(min-width:320px)",
       //   "ml":"(min-width:768px)",
       //   "big":"(min-width:1400px)",
       //   "mx":"(min-width:1920px)"
       // }
-
     },
   },
   plugins: [],
