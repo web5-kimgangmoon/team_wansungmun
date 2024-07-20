66d1caf934f43982cfa8616b4b6e583b678fda27
JiwanBang <jiwanbang@gmail.com>
2024-07-20 18:51:12 +0900

    0720

--- back/controllers/logCheck/index.ts
+++ back/controllers/logCheck/index.ts
@@ -1,11 +1,10 @@
 import User from "../../models/sequelize/user";
 import { Request, Response } from "express";
-import session from "express-session";
 
 export const logCheck = async (req: Request, res: Response) => {
   try {
     if (req.session) {
-      req.session.id = await User.findAll({
+      const body = await User.findAll({
         where: { id: req.session.user },
         include: [
           {
