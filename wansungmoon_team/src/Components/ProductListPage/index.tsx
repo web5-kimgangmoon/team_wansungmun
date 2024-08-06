54b70cd7c594a60750ca81bf82b8927f4aedb679
kimgangmoon <rkdans2111@naver.com>
2024-08-06 19:42:38 +0900

    feat:testObj

--- wansungmoon_team/src/Components/ProductListPage/index.tsx
+++ wansungmoon_team/src/Components/ProductListPage/index.tsx
@@ -10,10 +10,8 @@ const ProductListPage = () => {
 
   const [isDirectCheck, setIsDirectCheck] = useState<boolean>(true);
   if (isPending || isFetching) return <div>로딩중</div>;
-  if (!data || !data.data || data.data === undefined)
+  if (!data || !data.data || typeof data.data === "object")
     return <div>데이터가 존재하지 않습니다</div>;
-  console.log(data);
-  console.log(data.data);
   const itemArr: DataTy[] = [];
   for (let item of data.data.products) {
     if (isDirectCheck && !item.isDirectTrade) continue;
