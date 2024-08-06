ff574998541e8e01470ad15afa01295f1cfbca38
kimgangmoon <kimgangmoon>
2024-08-06 13:55:16 +0900

    feat:front

--- wansungmoon_team/src/Components/Public/Body/KakaoMapLocationGetter/hooks/useKakaoLoader.ts
+++ wansungmoon_team/src/Components/Public/Body/KakaoMapLocationGetter/hooks/useKakaoLoader.ts
@@ -2,7 +2,7 @@ import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk";
 
 export default function useKakaoLoader() {
   useKakaoLoaderOrigin({
-    appkey: "ffc8b4a023164e322ab4909015e6231f", //본인 앱 키를 넣어주자.
+    appkey: process.env.REACT_APP_KAKAOKEY as string, //본인 앱 키를 넣어주자.
     libraries: ["clusterer", "drawing", "services"],
   });
 }
