389d82a439b979620c7d1e88965ddc2c7cfd099f
kimgangmoon <162942474+web5-kimgangmoon@users.noreply.github.com>
2025-02-03 18:16:25 +0900

    feat:unsuccess

--- wansungmoon_team/src/index.tsx
+++ wansungmoon_team/src/index.tsx
@@ -32,7 +32,7 @@ const queryClient = new QueryClient({
 });
 root.render(
   // <React.StrictMode>
-  <BrowserRouter basename="https://web5-kimgangmoon.github.io/team_wansungmun/build/">
+  <BrowserRouter>
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <App />
