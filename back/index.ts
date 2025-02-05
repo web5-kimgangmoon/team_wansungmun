37b81da73651367cb5d95cec0df5d3c2b027a1b3
kim gang moon <rkdans2111@naver.com>
2025-02-05 17:08:36 +0900

    fix:search problem, success. that's ec2 public ip address problem

--- back/index.ts
+++ back/index.ts
@@ -12,8 +12,8 @@ import fileStore from "session-file-store";
 
 import mkCase from "./queries/testCase";
 
-// dotenv.config({ path: `${__dirname}/.env` });
-dotenv.config();
+dotenv.config({ path: `${__dirname}/.env` });
+// dotenv.config();
 
 const app = express();
 const { server } = useSocket(app);
@@ -48,8 +48,8 @@ app.use(
     name: "user",
     store: new FileStore({
       reapInterval: 1000,
-      // path: `${__dirname}/user_session`,
-      path: "./user_session",
+      path: `${__dirname}/user_session`,
+      // path: "./user_session",
     }),
     cookie: {
       maxAge: 20 * 60 * 1000,
